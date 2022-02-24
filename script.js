4e918c09556c4f1bb9b4aa91a4634ebc300ddb74
SimoAl17 <simo.al.17w@gmail.com>
2022-02-24 12:10:52 +0100

    events V1.1
    Mouse events, intervals and timeout.

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 
+
+let intervalCounter = 0;
+const interval = setInterval(() => {
+    console.log("Pluto ogni secondo fino a 10");
+    if (intervalCounter === 9) {
+        clearInterval(interval);
+    }
+    intervalCounter++;
+}, 1000);
+
+setTimeout(() => {
+    console.log("Pippo dopo 10 secondi");
+}, 10000);
+
+
 const button3 = document.getElementById('button3');
 button3.onclick = () => buttonClick('button 3');
 
@@ -6,6 +21,9 @@ button3.onclick = countClick;
 
 const button4 = document.getElementById('button4');
 button4.addEventListener('mousedown', buttonClick);
+document.addEventListener('keydown', onKeyDown);
+
+
 
 const parPippo = document.getElementById('par-pippo');
 parPippo.addEventListener('click', countClick);
@@ -17,9 +35,12 @@ function buttonClick(string) {
 const link = document.getElementById('link');
 link.addEventListener('click', displayPar);
 
-document.addEventListener('keydown', changeBackground)
+document.addEventListener('keydown', changeBackground);
 
 
+const div = document.getElementById('tomato');
+div.addEventListener('mouseenter', onMouseEnter);
+div.addEventListener('mouseleave', onMouseLeave);
 
 let counter = 0;
 
@@ -29,7 +50,7 @@ function countClick(event) {
     //if (counter === 7) {
     //    button4.removeEventListener('click', countClick)
     //}
-    event.stopPropagation();   //evita di propagare il click in caso di elementi sovrapposti
+    event.stopPropagation();    //evita di propagare il click in caso di elementi sovrapposti
     
 }
 
@@ -45,4 +66,30 @@ function changeBackground(event) {
         event.preventDefault();
         document.body.style.backgroundColor = '#' + number + number + number + number + number + number;
     }
+}
+
+function onMouseEnter() {
+    console.log("Sono entrato");
+}
+
+function onMouseLeave() {
+    console.log("Sono uscito");
+}
+
+let selectedKey = "";
+function onKeyDown(event) {
+    if (event.key !== selectedKey) {
+        console.log("Tasto premuto: " + event.key);
+        selectedKey = event.key;    
+    }
+}
+
+let searchTimeout;
+function realTimeSearch(event) {
+    if (searchTimeout) {
+        clearTimeout(searchTimeout);
+    }
+    searchTimeout = setTimeout(() => {
+        console.log("Sto cercando");
+    }, 1000);
 }
